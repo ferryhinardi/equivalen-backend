693edea209d2a39f0ec3bf57081a72a75ac88ce8
Jacky Wijaya <jekiwijaya@hotmail.com>
2018-08-25 13:57:13 +0700

    google cloud config. circle ci config.

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import models from './models';
  * DISCLAIMER: using sequelize#sync is not recommended for production use. Please, please
  * use migrations. This method of creating a database is used in this demo for simplicity's sake.
  */
+const port = process.env.PORT || '4000';
 async function start() {
   // Start the GraphQL server
   models.sequelize.sync().then(() => {
-    server.start(() => {
-      console.log('Server is running on localhost:4000');
+    server.start(({
+      port
+    }) => {
+      console.log(`Server is running on localhost:${port}`);
     });
   });
 }
--- src/server/context/index.js
+++ src/server/context/index.js
@@ -3,7 +3,13 @@ import { createContext, EXPECTED_OPTIONS_KEY } from 'dataloader-sequelize';
 
 export default function context(req) {
   const dataloaderContext = createContext(models.sequelize);
+  const headers = req.request.headers;
+  let token = null;
+  if (headers['authorization']) {
+    token = headers['authorization'].replace('Bearer ', '');
+  }
   return {
     [EXPECTED_OPTIONS_KEY]: dataloaderContext,
+    token,
   };
 }
