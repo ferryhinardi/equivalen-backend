71a600ee295bcdb719b885b83b4809e36cbf40da
Jacky Wijaya <jekiwijaya@hotmail.com>
2018-08-28 23:25:19 +0700

    enable express cors

--- src/server/index.js
+++ src/server/index.js
@@ -2,11 +2,21 @@ import { GraphQLServer } from 'graphql-yoga';
 import typeDefs from './typeDefs';
 import resolvers from './resolvers';
 import context from './context';
+import cors from 'cors';
 
 const server = new GraphQLServer({
   typeDefs,
   resolvers,
   context
+});
+
+server.express.use(cors({
+  optionsSuccessStatus: 200,
+}));
+server.express.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
 });
 
 export default server;
