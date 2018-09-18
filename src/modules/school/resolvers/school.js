80eb55f424ca3fda86461b60bc496aa69d01b359
Jacky Wijaya <jekiwijaya@hotmail.com>
2018-09-19 00:46:18 +0700

    add docs.

--- src/modules/school/resolvers/school.js
+++ src/modules/school/resolvers/school.js
@@ -1,8 +1,19 @@
 import resolver from 'modules/shared/libs/graphql-sequelize/resolver';
-import { School } from 'models';
+import { School, Sequelize } from 'models';
 
 export default {
   Query: {
-    schools: resolver(School)
+    schools: resolver(School, {
+      before: (findOption, args) => {
+        if (args.keyword) {
+          findOption.where = {
+            name: {
+              [Sequelize.Op.like]: `${args.keyword}%`
+            }
+          };
+        }
+        return findOption;
+      }
+    })
   }
 };
