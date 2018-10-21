cd4fb2d5b3455a7f4226e714213110e7486b337f
Jacky Wijaya <jekiwijaya@hotmail.com>
2018-10-22 02:46:30 +0700

    add debugging simulate payment

--- src/modules/payment/integrations/xendit/simulatePayment.js
+++ src/modules/payment/integrations/xendit/simulatePayment.js
@@ -8,7 +8,7 @@ export default async function simulatePaymentUsingAlamart(order) {
       available_retail_outlets: [{ payment_code }]
     }
   } = await getInvoice({ invoiceId: invoice.invoiceId });
-
+  console.log(`simulate ${payment_code} ${order.totalPrice}`);
   return request.post('/payment_code/simulate_payment!', {
     retail_outlet_name: 'ALFAMART',
     payment_code,
