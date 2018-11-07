d694796cabe329cd8f2f2afd91c558fc8b37f08c
Ferry Hinardi <ferry_hinardi@yahoo.co.id>
2018-11-07 10:25:42 +0700

    fixing error mysql sequelize where

--- src/modules/order/controllers/reindexXenditInvoice.js
+++ src/modules/order/controllers/reindexXenditInvoice.js
@@ -5,16 +5,12 @@ const debug = require('debug')('app:webhook:xendit');
 export default async function reindexXenditInvoice(req, res) {
   const invoiceId = req.body.id;
   debug(`Incoming webhook: invoice-id: ${invoiceId}`);
-  const orderXenditInvoiceDebug = await OrderXenditInvoice.findOne({
+  const orderXenditInvoice = await OrderXenditInvoice.findOne({
     where: {
       invoiceId
     }
   });
-  debug(`Incoming webhook: orderXenditInvoiceDebug: ${orderXenditInvoiceDebug}`);
-  const orderXenditInvoice = await OrderXenditInvoice.findOne({
-    invoiceId
-  });
-  debug(`Incoming webhook: orderXenditInvoice: ${orderXenditInvoice}`);
+  debug(`Incoming webhook: orderXenditInvoice - debug: ${JSON.stringify(orderXenditInvoice)}`);
   const order = await orderXenditInvoice.getOrder();
   await order.reindex();
   await order.reload();
