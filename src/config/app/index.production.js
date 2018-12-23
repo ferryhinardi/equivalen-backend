83bc64b8c35c9ec45989c56154fc12641e8a8f54
Ferry Hinardi <ferry_hinardi@yahoo.co.id>
2018-12-23 22:24:01 +0700

    Update index.production.js

--- src/config/app/index.production.js
+++ src/config/app/index.production.js
@@ -4,8 +4,8 @@ export const API_HOST = 'localhost';
 export const API_PORT = '4000';
 export const API_URL = `http://${API_HOST}:${API_PORT}`;
 
-export const FACEBOOK_APP_ID = '269466223664135';
-export const FACEBOOK_ACCOUNT_KIT_APP_SECRET = '4e612d9639608cd31b5d669f88daf129';
-export const FACEBOOK_ACCOUNT_KIT_CLIENT_TOKEN = '89f418f5d609aa74f7b36bc967a18803';
-export const FACEBOOK_ACCOUNT_KIT_VERSION = 'v1.0';
+export const FACEBOOK_APP_ID = '2281886948759780';
+export const FACEBOOK_ACCOUNT_KIT_APP_SECRET = '4f1cdd68f16c1b773eca65aa67bc6004';
+export const FACEBOOK_ACCOUNT_KIT_CLIENT_TOKEN = '17bff36ceee204a7af9c28ce0c97edbb';
+export const FACEBOOK_ACCOUNT_KIT_VERSION = 'v1.1';
 export const ENGINE_API_KEY = 'service:equivalen-prod:y2_r644zvzmr50KTrJ-u1Q';
