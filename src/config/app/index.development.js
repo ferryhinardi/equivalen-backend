62d8b06a6a688d57fcd2f72327ce363012374e89
Ferry Hinardi <ferry_hinardi@yahoo.co.id>
2018-12-23 22:37:05 +0700

    fix config

--- src/config/app/index.development.js
+++ src/config/app/index.development.js
@@ -7,5 +7,5 @@ export const API_URL = `http://${API_HOST}:${API_PORT}`;
 export const FACEBOOK_APP_ID = '269466223664135';
 export const FACEBOOK_ACCOUNT_KIT_APP_SECRET = '4e612d9639608cd31b5d669f88daf129';
 export const FACEBOOK_ACCOUNT_KIT_CLIENT_TOKEN = '89f418f5d609aa74f7b36bc967a18803';
-export const FACEBOOK_ACCOUNT_KIT_VERSION = 'v1.0';
+export const FACEBOOK_ACCOUNT_KIT_VERSION = 'v1.1';
 export const ENGINE_API_KEY = 'service:equivalen-stag:N1gY4P4KNJY7g0AqtjSfXw';
