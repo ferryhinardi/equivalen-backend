d04c27f5bc919bc92661cb6e789a88e7cfd861df
Ferry Hinardi <ferry_hinardi@yahoo.co.id>
2019-01-17 14:34:05 +0700

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -15,6 +15,16 @@ module.exports = {
     operatorsAliases: false,
     quoteIdentifiers: false // set case-insensitive
   },
+  staging: {
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_DATABASE || 'equivalent',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'mysql',
+    logging: false,
+    operatorsAliases: false,
+    quoteIdentifiers: false // set case-insensitive
+  },
   production: {
     username: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
