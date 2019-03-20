1cdf995675d10b2189e1e8a82a68aee36cef3e30
Ferry Hinardi <ferry_hinardi@yahoo.co.id>
2019-03-20 18:27:04 +0700

    fixing error migration

--- src/migrations/007_add_column_bod_table_users.js
+++ src/migrations/007_add_column_bod_table_users.js
@@ -1,16 +1,18 @@
 export default {
-  up: (queryInterface, Sequelize) => {
-    queryInterface.addColumn('users', 'place_bod', {
-      type: Sequelize.STRING,
-      allowNull: true
-    });
-    queryInterface.addColumn('users', 'date_bod', {
-      type: Sequelize.DATE,
-      allowNull: true
-    });
-  },
-  down: queryInterface => {
-    queryInterface.removeColumn('users', 'place_bod');
-    queryInterface.removeColumn('users', 'date_bod');
-  }
+  up: (queryInterface, Sequelize) =>
+    Promise.resolve(() => {
+      queryInterface.addColumn('users', 'place_bod', {
+        type: Sequelize.STRING,
+        allowNull: true
+      });
+      queryInterface.addColumn('users', 'date_bod', {
+        type: Sequelize.DATE,
+        allowNull: true
+      });
+    }),
+  down: queryInterface =>
+    Promise.resolve(() => {
+      queryInterface.removeColumn('users', 'place_bod');
+      queryInterface.removeColumn('users', 'date_bod');
+    })
 };
