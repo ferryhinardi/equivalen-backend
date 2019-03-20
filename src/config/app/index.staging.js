b5c0f2cd2a2dbd3d1adb28c927de7a8244d8eb41
Ferry Hinardi <ferry_hinardi@yahoo.co.id>
2019-03-20 15:05:32 +0700

    add debug file

--- src/config/app/index.staging.js
+++ src/config/app/index.staging.js
@@ -6,4 +6,4 @@ export const FACEBOOK_APP_ID = '2281886948759780';
 export const FACEBOOK_ACCOUNT_KIT_APP_SECRET = '4f1cdd68f16c1b773eca65aa67bc6004';
 export const FACEBOOK_ACCOUNT_KIT_CLIENT_TOKEN = '17bff36ceee204a7af9c28ce0c97edbb';
 export const FACEBOOK_ACCOUNT_KIT_VERSION = 'v1.1';
-export const ENGINE_API_KEY = 'service:equivalen-prod:y2_r644zvzmr50KTrJ-u1Q';
+export const ENGINE_API_KEY = 'service:equivalen-stag:N1gY4P4KNJY7g0AqtjSfXw';
